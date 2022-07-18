07b001f32110eb7cd197ea03f8ae9fdb5027d713
Gabriel Soares <soaresggm@gmail.com>
2022-07-18 02:36:03 -0300

    fix display number in sh script

--- scripts/executor.sh
+++ scripts/executor.sh
@@ -9,20 +9,20 @@ threads=$(($th*4))
 for (( w=0; w<$th; w++ ))
 do
     ex=$(($w*4+1))
-    tmux send-keys -t tsprd:$w "echo \"Thread 1/${threads}\" && julia executor.jl ${outputfolder} ${threads} ${ex}" C-m
+    tmux send-keys -t tsprd:$w "echo \"Thread ${ex}/${threads}\" && julia executor.jl ${outputfolder} ${threads} ${ex}" C-m
 
     ex=$(($w*4+2))
     tmux split-window -t tsprd:$w -h -p 50
-    tmux send-keys -t tsprd:$w "echo \"Thread 2/${threads}\" && julia executor.jl ${outputfolder} ${threads} ${ex}" C-m
+    tmux send-keys -t tsprd:$w "echo \"Thread ${ex}/${threads}\" && julia executor.jl ${outputfolder} ${threads} ${ex}" C-m
 
     ex=$(($w*4+4))
     tmux split-window -t tsprd:$w -v -p 50
-    tmux send-keys -t tsprd:$w "echo \"Thread 4/${threads}\" && julia executor.jl ${outputfolder} ${threads} ${ex}" C-m
+    tmux send-keys -t tsprd:$w "echo \"Thread ${ex}/${threads}\" && julia executor.jl ${outputfolder} ${threads} ${ex}" C-m
 
     ex=$(($w*4+3))
     tmux select-pane -t 0
     tmux split-window -t tsprd:$w -v -p 50
-    tmux send-keys -t tsprd:$w "echo \"Thread 3/${threads}\" && julia executor.jl ${outputfolder} ${threads} ${ex}" C-m
+    tmux send-keys -t tsprd:$w "echo \"Thread ${ex}/${threads}\" && julia executor.jl ${outputfolder} ${threads} ${ex}" C-m
 
     tmux new-window -t tsprd
 done
